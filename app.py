b0614671cffba52586bcd5b3cb5026f4e2fd96c9
Devansh <devanshprataps6@gmail.com>
2026-07-16 22:39:28 +0530

    fixed app.py, html and css

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-import gzip
 import pickle
 import pandas as pd
-import numpy as np
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template
 
 app = Flask(__name__, static_folder='static', static_url_path='/static')
 
@@ -29,17 +27,26 @@ def predict():
             form_data = request.form.to_dict()
 
             input_data = []
+
             for col in original_columns:
                 val = form_data.get(col)
+
                 if val is None:
-                    print("Post_Request_None")
-                    return render_template("index.html", error=f"Missing value for {col}")
-                
+                    return render_template(
+                        "index.html",
+                        error=f"Missing value for {col}"
+                    )
+
                 try:
-                    float_val = float(val)
-                    input_data.append(int(float_val) if float_val.is_integer() else round(float_val, 2))
+                    num = float(val)
+
+                    if num.is_integer():
+                        input_data.append(int(num))
+                    else:
+                        input_data.append(num)
+
                 except ValueError:
-                    print("Error")
+                    input_data.append(val)
 
             # Convert to DataFrame
             df_data = pd.DataFrame([input_data], columns=original_columns)
@@ -47,16 +54,25 @@ def predict():
             # Apply preprocessing transformation
             pre_data = preprocessing_pipeline.transform(df_data)
 
-            # Make prediction
-            prediction = model.predict(pre_data)
-            output = int(prediction[0])
-            print("output",output)
-            result = ""
-            if output == 1:
-                result =  "The customer will subscribe to the term deposit."
+           # Make prediction
+            prediction = model.predict(pre_data)[0]
+            probability = model.predict_proba(pre_data)[0]
+            confidence = round(probability[int(prediction)] * 100, 2)
+
+            output = int(prediction)
+            print("output", output)
+
+            if prediction == 1:
+                result = "The customer will subscribe to the term deposit."
             else:
                 result = "The customer will not subscribe to the term deposit."
-            return render_template("index.html",isSub=result)    
+
+
+            return render_template(
+                "index.html",
+                isSub=result,
+                confidence=confidence
+            )    
 
 
         except Exception as e:
